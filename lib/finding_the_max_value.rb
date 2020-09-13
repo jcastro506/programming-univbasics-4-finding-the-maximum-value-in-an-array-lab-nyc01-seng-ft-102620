cbde581e455dd2f773fcf15bfe6fa9bf38a8ae3b
breezy-builder-9254 <castrojoshua805@gmail.com>
2020-09-13 17:01:03 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -2,8 +2,8 @@ def find_max_value(array)
   count = 0 
   highest_num = 0 
   while count < array.length do 
-    if count > highest_num
-      highest_num == count 
+    if highest_num < array[count]
+      highest_num == array[count] 
     end 
     count++
   end
